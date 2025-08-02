fbe85b650a1f5faccae4d20920f8419f26a39038
zh1kaa <ryskulovazibek21@gmail.com>
2025-08-02 17:00:07 +0600

    test

--- src/api/popular/types.d.ts
+++
@@ -1,4 +0,0 @@
-namespace POPULAR {
-	type GetPopularRes = IMovies;
-	type GetPopularReq = 'movie' | 'tv';
-}
--- src/api/top-rated/types.d.ts
+++
@@ -1,4 +0,0 @@
-namespace TOP_RATED {
-	type GetTopRatedRes = IMovies;
-	type GetTopRatedReq = 'movie' | 'tv';
-}

e718b2bc3109f2bb82f08d1c1c45820f16289b82
zh1kaa <ryskulovazibek21@gmail.com>
2025-08-02 17:01:14 +0600

    test

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,15 +7,3 @@ export const api_tmdb = axios.create({
 			'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNDRhMDEwNTljZTg5MDlmYzgxMTQxYjk3MGJjMzhjYyIsIm5iZiI6MTcwMjgwMTIzMS4yMDUsInN1YiI6IjY1N2VhZjRmMDcyOTFjMDdkZjNlZjIzZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5nJEcU0w0TpoZTgi20a6mQ_ftNMQPbvmqQsjvGLENGM'
 	}
 });
-
-// export const api = browser
-// 	? // Клиентская конфигурация
-// 	  axios.create({
-// 			baseURL: "https://1116a39bcd510908.mokky.dev",
-// 			timeout: 5000,
-// 	  })
-// 	: // Серверная конфигурация
-// 	  axios.create({
-// 			baseURL: "http://localhost:5173/api",
-// 			timeout: 10000,
-// 	  });
